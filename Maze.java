63ded01abc4d39c19732254587fddb63480a129a
Alvi Aziz <aaziz00@stuy.edu>
2019-02-27 15:14:35 -0500

    constructor and toString

---
+++ Maze.java
@@ -0,0 +1,115 @@
+import java.util.*;
+import java.io.*;
+public class Maze{
+    private char[][] maze;
+    private boolean animate;//false by default
+
+    /*Constructor loads a maze text file, and sets animate to false by default.
+
+      1. The file contains a rectangular ascii maze, made with the following 4 characters:
+      '#' - Walls - locations that cannot be moved onto
+      ' ' - Empty Space - locations that can be moved onto
+      'E' - the location of the goal (exactly 1 per file)
+      'S' - the location of the start(exactly 1 per file)
+
+      2. The maze has a border of '#' around the edges. So you don't have to check for out of bounds!
+
+      3. When the file is not found OR the file is invalid (not exactly 1 E and 1 S) then:
+         throw a FileNotFoundException or IllegalStateException
+    */
+    public Maze(String filename) throws FileNotFoundException{
+      animate = false;
+      File text = new File(filename);
+      Scanner inf = new Scanner(text);
+
+      int rows = 0;
+      int cols = 0;
+      while(inf.hasNextLine()){
+          String line = inf.nextLine();
+          rows++;
+          cols = line.length();
+      }
+
+      inf = new Scanner(text);
+      maze = new char[rows][cols];
+      int r = 0;
+      while(inf.hasNextLine()){
+        String line = inf.nextLine();
+        for (int c = 0 ; c < line.length() ; c++){
+          maze[r][c] = line.charAt(c);
+        }
+        r++;
+      }
+
+    }
+
+    public String toString(){
+      String ans = "";
+      for (int r = 0 ; r < maze.length ; r++){
+        for (int c = 0 ; c < maze[r].length ; c++){
+          ans += maze[r][c];
+        }
+        ans += "\n";
+      }
+      return ans;
+    }
+
+    private void wait(int millis){
+         try {
+             Thread.sleep(millis);
+         }
+         catch (InterruptedException e) {
+         }
+     }
+
+    public void setAnimate(boolean b){
+        animate = b;
+    }
+
+    public void clearTerminal(){
+        //erase terminal, go to top left of screen.
+        System.out.println("\033[2J\033[1;1H");
+    }
+
+
+    /*Wrapper Solve Function returns the helper function
+      Note the helper function has the same name, but different parameters.
+      Since the constructor exits when the file is not found or is missing an E or S, we can assume it exists.
+    */
+    public int solve(){
+            //find the location of the S.
+
+            //erase the S
+
+            //and start solving at the location of the s.
+            //return solve(???,???);
+            return -1;
+    }
+
+    /*
+      Recursive Solve function:
+
+      A solved maze has a path marked with '@' from S to E.
+
+      Returns the number of @ symbols from S to E when the maze is solved,
+      Returns -1 when the maze has no solution.
+
+      Postcondition:
+        The S is replaced with '@' but the 'E' is not.
+        All visited spots that were not part of the solution are changed to '.'
+        All visited spots that are part of the solution are changed to '@'
+    */
+    private int solve(int row, int col){ //you can add more parameters since this is private
+
+        //automatic animation! You are welcome.
+        if(animate){
+            clearTerminal();
+            System.out.println(this);
+            wait(20);
+        }
+
+        //COMPLETE SOLVE
+        return -1; //so it compiles
+    }
+
+}
---
+++ driver.java
@@ -0,0 +1,18 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+public class driver{
+    public static void main(String[]args){
+      String filename = "data1.dat";
+      try{
+        Maze f;
+        f = new Maze(filename);//true animates the maze.
+
+        //f.setAnimate(true);
+        //f.solve();
+        System.out.println(f);
+      }catch(FileNotFoundException e){
+        System.out.println("Invalid filename: "+filename);
+      }
+    }
+}
